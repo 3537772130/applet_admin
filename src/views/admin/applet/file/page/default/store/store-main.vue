7165529eac59493a26701fccc050a0a832e218ed
zhouhuahu123 <467820563@qq.com>
2019-10-17 00:47:31 +0800

    页面元素+

--- src/views/admin/applet/file/page/default/store/store-main.vue
+++ src/views/admin/applet/file/page/default/store/store-main.vue
@@ -18,33 +18,153 @@
       <div class="center-menu" :style="{height: contentHeight + 'px'}">
         <el-image src="/static/images/timg.jpg" class="center-img"></el-image>
         <div class="center-content">
-          <div>
-            <el-divider content-position="center"><span class="c-divider">分割线标题</span></el-divider>
+          <div class="img-carousel">
+            <el-carousel :interval="3000" arrow="never" height="150px" indicator-position="none">
+              <el-carousel-item>
+                <el-image src="/static/images/carousel/carousel-1.png" style="width: 100%;height: 150px;"></el-image>
+              </el-carousel-item>
+              <el-carousel-item>
+                <el-image src="/static/images/carousel/carousel-2.png?123" style="width: 100%;height: 150px;"></el-image>
+              </el-carousel-item>
+              <el-carousel-item>
+                <el-image src="/static/images/carousel/carousel-3.png" style="width: 100%;height: 150px;"></el-image>
+              </el-carousel-item>
+              <el-carousel-item>
+                <el-image src="/static/images/carousel/carousel-4.png" style="width: 100%;height: 150px;"></el-image>
+              </el-carousel-item>
+              <el-carousel-item>
+                <el-image src="/static/images/carousel/carousel-5.png" style="width: 100%;height: 150px;"></el-image>
+              </el-carousel-item>
+            </el-carousel>
+          </div>
+          <div class="text-move">
+            <div class="text-content">
+              轰烈流沙枕上白发,杯中酒比划,年少风雅鲜衣怒马,也不过一刹那,难免疏漏儿时檐下,莫测变化,隔却山海,转身时从容煎茶。
+            </div>
+            <div :style="moveCoverStyle">
+              <div class="cover-left" :style="cover_1"><div></div></div>
+              <div :style="cover_2"></div>
+              <div class="cover-right" :style="cover_1"><div></div></div>
+            </div>
+            <el-image class="text-trumpet" src="/static/images/icon/trumpet.png"></el-image>
+          </div>
+          <div class="type-frame">
+            <div class="type-element">
+              <div><el-image class="type-icon" src="/static/images/icon/shopping.png"></el-image></div>
+              <span class="type-name">type1</span>
+            </div>
+            <div class="type-element">
+              <div><el-image class="type-icon" src="/static/images/icon/shopping.png"></el-image></div>
+              <span class="type-name">type2</span>
+            </div>
+            <div class="type-element">
+              <div><el-image class="type-icon" src="/static/images/icon/shopping.png"></el-image></div>
+              <span class="type-name">type3</span>
+            </div>
+            <div class="type-element">
+              <div><el-image class="type-icon" src="/static/images/icon/shopping.png"></el-image></div>
+              <span class="type-name">type4</span>
+            </div>
+            <div class="type-element">
+              <div><el-image class="type-icon" src="/static/images/icon/shopping.png"></el-image></div>
+              <span class="type-name">type5</span>
+            </div>
+            <div class="type-element">
+              <div><el-image class="type-icon" src="/static/images/icon/shopping.png"></el-image></div>
+              <span class="type-name">type6</span>
+            </div>
+            <div class="type-element">
+              <div><el-image class="type-icon" src="/static/images/icon/shopping.png"></el-image></div>
+              <span class="type-name">type7</span>
+            </div>
+            <div class="type-element">
+              <div><el-image class="type-icon" src="/static/images/icon/shopping.png"></el-image></div>
+              <span class="type-name">type8</span>
+            </div>
           </div>
           <div class="img-title">
             <el-image src="/static/images/icon/shopping.png" class="img-title-image"></el-image>
             <span class="img-title-text">拼团更实惠<span> | <span>团购促销，多买多降</span></span></span>
           </div>
-          <div class="img-carousel">
-            <el-carousel :interval="3000" arrow="never" height="220px" indicator-position="none">
-              <el-carousel-item>
-                <img src="/static/images/carousel/zhao-shang.png" style="width: 100%;height: 220px;">
-              </el-carousel-item>
-              <el-carousel-item>
-                <img src="/static/images/carousel/zhe-shang.png" style="width: 100%;height: 220px;">
-              </el-carousel-item>
-              <el-carousel-item>
-                <img src="/static/images/carousel/zhong-guo.png" style="width: 100%;height: 220px;">
-              </el-carousel-item>
-            </el-carousel>
-          </div>
           <div class="img-ordinary">
-            <el-image src="/static/images/carousel/zhong-guo.png" style="width: 100%;height: 140px;"></el-image>
+            <el-image src="/static/images/carousel/carousel-6.png" style="width: 100%;height: 120px;"></el-image>
           </div>
-          <div class="text-move">
-            <div class="text-content"
-                 style="transition: transform 13542ms linear 0ms;transform: translateX(-541.68px);transform-origin: 50% 50% 0px;">
-              轰烈流沙枕上白发,杯中酒比划,年少风雅鲜衣怒马,也不过一刹那,难免疏漏儿时檐下,莫测变化,隔却山海,转身时从容煎茶。
+          <div class="goods-two-row">
+            <div class="goods-def">
+              <div>
+                <el-image class="goods-img" src="/static/images/goods/goods-1.png"></el-image>
+              </div>
+              <div class="goods-name">goods-name1发送飒飒大师sd水电费阿斯顿撒</div>
+              <div class="goods-price">￥188.88</div>
+            </div>
+            <div class="goods-def">
+              <div>
+                <el-image class="goods-img" src="/static/images/goods/goods-1.png"></el-image>
+              </div>
+              <div class="goods-name">goods-name2</div>
+              <div class="goods-price">￥13288.88</div>
+            </div>
+            <div class="goods-def">
+              <div>
+                <el-image class="goods-img" src="/static/images/goods/goods-1.png"></el-image>
+              </div>
+              <div class="goods-name">goods-name3</div>
+              <div class="goods-price">￥1088.88</div>
+            </div>
+            <div class="goods-def">
+              <div>
+                <el-image class="goods-img" src="/static/images/goods/goods-1.png"></el-image>
+              </div>
+              <div class="goods-name">goods-name4</div>
+              <div class="goods-price">￥98.88</div>
+            </div>
+          </div>
+          <div class="divider-title">
+            <div class="divider-line"></div>
+            <span class="c-divider">分割线标题</span>
+          </div>
+          <div class="goods-three-row">
+            <div class="goods-def">
+              <div>
+                <el-image class="goods-img" src="/static/images/goods/goods-1.png"></el-image>
+              </div>
+              <div class="goods-name">goods-name1发送飒飒大师sd水电费阿斯顿撒</div>
+              <div class="goods-price">￥188.88</div>
+            </div>
+            <div class="goods-def">
+              <div>
+                <el-image class="goods-img" src="/static/images/goods/goods-1.png"></el-image>
+              </div>
+              <div class="goods-name">goods-name2</div>
+              <div class="goods-price">￥13288.88</div>
+            </div>
+            <div class="goods-def">
+              <div>
+                <el-image class="goods-img" src="/static/images/goods/goods-1.png"></el-image>
+              </div>
+              <div class="goods-name">goods-name3</div>
+              <div class="goods-price">￥1088.88</div>
+            </div>
+            <div class="goods-def">
+              <div>
+                <el-image class="goods-img" src="/static/images/goods/goods-1.png"></el-image>
+              </div>
+              <div class="goods-name">goods-name4</div>
+              <div class="goods-price">￥98.88</div>
+            </div>
+            <div class="goods-def">
+              <div>
+                <el-image class="goods-img" src="/static/images/goods/goods-1.png"></el-image>
+              </div>
+              <div class="goods-name">goods-name5</div>
+              <div class="goods-price">￥168.88</div>
+            </div>
+            <div class="goods-def">
+              <div>
+                <el-image class="goods-img" src="/static/images/goods/goods-1.png"></el-image>
+              </div>
+              <div class="goods-name">goods-name6</div>
+              <div class="goods-price">￥2388.88</div>
             </div>
           </div>
         </div>
@@ -57,13 +177,37 @@
 
   export default {
     name: 'applet-element',
+    components: {
+    },
     data () {
       return {
         contentHeight: `${document.documentElement.scrollHeight - 150}`,
+        contentWidth: `${document.documentElement.scrollWidth}`,
         loading: false,
         list: [],
         contentList: [],
-        trans: 0
+        coverWidth: 0,
+        moveCoverStyle: {
+          'width': `${document.documentElement.scrollWidth}` + 'px',
+          'height': '30px',
+          'position': 'relative',
+          'left': (0 - (`${(document.documentElement.scrollWidth - 395)/2}`)) + 'px',
+          'top': '-30px',
+          'text-align': 'left'
+        },
+        cover_1: {
+          'width': (`${(document.documentElement.scrollWidth - 385)/2-16}`) + 'px',
+          'height': '30px',
+          'display': 'inline-block',
+          'background-color': '#FFFFFF'
+        },
+        cover_2: {
+          'width': '377px',
+          'height': '30px',
+          'display': 'inline-block',
+          'margin-left': '5px',
+          'margin-right': '7px'
+        }
       }
     },
     created () {
